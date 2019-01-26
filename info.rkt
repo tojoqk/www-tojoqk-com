3293ab5ac04fb6d2ee0ab1f6e77a6dc5bdacfa19
Masaya Tojo <masaya@tojo.tokyo>
2019-01-27 02:52:19 +0900

    Add info.rkt

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "www-tojo-tokyo")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/www-tojo-tokyo.scrbl" ())))
+(define pkg-desc "My Web Site")
+(define version "0.0")
+(define pkg-authors '(tojoqk))
