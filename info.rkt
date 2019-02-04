07c074eef978950c1781778fb9902d19e06f94cc
Masaya Tojo <masaya@tojo.tokyo>
2019-02-04 23:19:15 +0900

    Rename www-tojo-tokyo to www-tojoqk-com

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 (define collection "www-tojoqk-com")
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/www-tojo-tokyo.scrbl" ())))
+(define scribblings '(("scribblings/www-tojoqk-com.scrbl" ())))
 (define pkg-desc "My Web Site")
 (define version "0.1")
 (define pkg-authors '(tojoqk))
