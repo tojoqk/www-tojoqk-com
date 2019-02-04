422dd68a6948f7784d2ed569452c4f179439381a
Masaya Tojo <masaya@tojo.tokyo>
2019-02-04 22:58:39 +0900

    Version up

--- info.rkt
+++ info.rkt
@@ -4,5 +4,5 @@
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/www-tojo-tokyo.scrbl" ())))
 (define pkg-desc "My Web Site")
-(define version "0.0")
+(define version "0.1")
 (define pkg-authors '(tojoqk))
