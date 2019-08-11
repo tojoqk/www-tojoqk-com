a8f3be839d671cac9db8044df41148976b4eebac
Masaya Tojo <masaya@tojo.tokyo>
2019-08-11 19:54:43 +0900

    About me ページを追加

--- servlet.rkt
+++ servlet.rkt
@@ -2,12 +2,14 @@
 (require web-server/http
          "page/home.rkt"
          "page/not-found.rkt"
+         "page/about-me.rkt")
 (provide start)
 
 (define-values (top-dispatch top-url)
   (dispatch-rules
    [("") (λ (req) (redirect-to "/home" permanently))]
    [("home") home]
+   [("about-me") about-me]
    [else not-found]))
 
 (define (start req)
