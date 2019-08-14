77b17c9a08c696f900a88a17b414096435e157c3
Masaya Tojo <masaya@tojo.tokyo>
2019-08-15 04:07:09 +0900

    home から Game への導線を配置

--- page/home.rkt
+++ page/home.rkt
@@ -15,5 +15,8 @@
      (p "ごゆっくりとお過ごしいただければ幸いです。")
      (h2 "Index")
      (ul
+      (li (h3 (a #:href "/game" "Game"))
+          (p "暇つぶしのために作ったゲーム")
+          (p "Racket の Web サーバーの勉強のために作成した"))
       (li (h3  (a #:href "/about-me" "About me"))
           (p "TojoQK に関する簡単な紹介")))))))
