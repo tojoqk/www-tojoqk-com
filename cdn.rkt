fdf2c32732c79e7da8cafff112ccec2bfbb00f8d
Masaya Tojo <masaya@tojo.tokyo>
2019-08-29 01:10:33 +0900

    CDN周りの処理に型を付ける

--- cdn.rkt
+++ cdn.rkt
@@ -1,11 +1,13 @@
-#lang racket/base
+#lang typed/racket/base
 (provide cdn/www-tojoqk-com)
 
-(define www-tojoqk-com
+(: www-tojoqk-com (-> String))
+(define (www-tojoqk-com)
   (cond
     [(getenv "WWW_TOJOQK_COM_CDN") => values]
     [else "dwlsypxdguxvw.cloudfront.net"]))
 
+(: cdn/www-tojoqk-com (-> String String))
 (define (cdn/www-tojoqk-com path)
-  (string-append "https://" www-tojoqk-com "/" path))
+  (string-append "https://" (www-tojoqk-com) "/" path))
 
