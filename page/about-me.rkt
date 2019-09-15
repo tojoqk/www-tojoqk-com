0f8e2e12326a0a423df5e151367406cbde46a280
Masaya Tojo <masaya@tojo.tokyo>
2019-09-15 12:05:01 +0900

    Fix about-me

--- page/about-me.rkt
+++ page/about-me.rkt
@@ -25,7 +25,9 @@
      (h1 "TojoQKについて")
      (p "QK には特に意味はありません。")
      (p (list
-         "プログラミング言語の " (a #:href "https://racket-lang.org/" "Racket")  " が好きです。"
+         "プログラミング言語の "
+         (a #:href "https://racket-lang.org/" "Racket")
+         " が好きです。"
          "この Web サイトも Racket で書かれています("
          (a #:href "https://github.com/tojoqk/www-tojoqk-com"
             "repo")
@@ -34,14 +36,20 @@
       #:class "sns-title"
       (list
        (h2 "Twitter")
-       (a #:class "sns-link" #:href "https://twitter.com/tojoqk" "@tojoqk")
-       (p "唯一利用している SNS ですが、投稿の頻度はそれほど高くありません……。")
-       (p (list
-           "このホームページについての問い合わせは、"
-           "Twitter で DM や Replay をしていただく形でお願いします。"))))
+       (div
+        (list
+         (a #:class "sns-link" #:href "https://twitter.com/tojoqk" "@tojoqk")
+         (p "唯一利用している SNS ですが、投稿の頻度はそれほど高くありません……。")
+         (p (list
+             "このホームページについての問い合わせは、"
+             "Twitter で DM や Replay をしていただく形でお願いします。"))))))
      (div
       #:class "sns-title"
       (list
        (h2 "GitHub")
-       (a #:class "sns-link" #:href "https://github.com/tojoqk/" "tojoqk")
-       (p "TojoQK がソースコードを公開する目的で使用している git のホスティングサービスです。")))))))
+       (div
+        (list
+         (a #:class "sns-link"
+            #:href "https://github.com/tojoqk/"
+            "tojoqk")
+         (p "TojoQK がソースコードを公開する目的で使用している git のホスティングサービスです。")))))))))
