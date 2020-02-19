fac428c230ac768ab57afab149c529334a94ba94
Masaya Tojo <masaya@tojo.tokyo>
2020-02-20 02:00:27 +0900

    www.tojoqk.com を終了する(www.tojo.tokyoへリダイレクト)

--- main.rkt
+++ main.rkt
@@ -1,62 +1,11 @@
 #lang racket
-(require json
-         "template.rkt"
-         "page/home.rkt"
-         "page/game.rkt"
-         "page/about-me.rkt"
-         "page/not-found.rkt"
-         "page/internal-server-error.rkt")
-(provide root
-         home
-         game
-         about-me
-         not-found)
 
-(module+ test
-  (require rackunit))
-
-(define host "127.0.0.1")
-(define port 8080)
+(provide root not-found)
 
 (define (root event)
   (hash 'statusCode 301
-        'headers (hash 'location "/home")))
+        'headers (hash 'location "https://www.tojo.tokyo/")))
 
-(define (page page/)
-  (lambda (event)
-    (with-handlers ([exn:fail?
-                     (lambda (e)
-                       (write-log event (page/internal-server-error)))])
-      (write-log event (page/ event)))))
-
-(define (write-log event response)
-  (let* ([request-context (hash-ref event 'requestContext)]
-         [identity (hash-ref request-context 'identity)])
-    (printf "{~a}~%"
-            (string-join
-             (map (λ (p) (format "\"~a\":~a"
-                                 (car p)
-                                 (jsexpr->string (cdr p))))
-                  (list (cons 'requestTime
-                              (hash-ref request-context 'requestTime))
-                        (cons 'requestTimeEpoch
-                              (hash-ref request-context 'requestTimeEpoch))
-                        (cons 'requestId (hash-ref request-context 'requestId))
-                        (cons 'method (hash-ref request-context 'httpMethod))
-                        (cons 'path (hash-ref request-context 'path))
-                        (cons 'queryStringParameters
-                              (hash-ref event 'queryStringParameters))
-                        (cons 'multiValueQueryStringParameters
-                              (hash-ref event
-                                        'multiValueQueryStringParameters))
-                        (cons 'statusCode
-                              (hash-ref response 'statusCode))
-                        (cons 'sourceIp (hash-ref identity 'sourceIp))
-                        (cons 'userAgent (hash-ref identity 'userAgent))))
-             ","))
-    response))
-
-(define home (page page/home))
-(define game (page page/game))
-(define about-me (page page/about-me))
-(define not-found (page page/not-found))
+(define (not-found event)
+  (hash 'statusCode 301
+        'headers (hash 'location "https://www.tojo.tokyo/")))
